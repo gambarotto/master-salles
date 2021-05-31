2f26315cac6298303ece54f84c939c54a7a4740a
gambarotto <dgcdeveloper@gmail.com>
2021-05-31 18:25:59 -0300

    refactor 2.2

--- src/modules/categories/infra/typeorm/repositories/CategoriesRepository.ts
+++ src/modules/categories/infra/typeorm/repositories/CategoriesRepository.ts
@@ -1,4 +1,16 @@
 import ICategoriesRepository from '@modules/categories/repositories/ICategoriesRepository';
 
-class CategoriesRepository implements ICategoriesRepository {}
+class CategoriesRepository implements ICategoriesRepository {
+  async create(name: string): Promise<Category> {}
+
+  async update(category: Category): Promise<Category> {}
+
+  async delete(category_id: string): Promise<void> {}
+
+  async findByName(name: string): Promise<Category | undefined> {}
+
+  async findById(category_id: string): Promise<Category | undefined> {}
+
+  async findAllCategories(): Promise<Category[]> {}
+}
 export default CategoriesRepository;
